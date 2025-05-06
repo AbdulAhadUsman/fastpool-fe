0fa5e918634eadb773c6f4a674ef5866f592489f
shariqmunir99 <shariq.munir7@gmail.com>
2025-05-06 20:38:33 +0500

    add past_trip model (Ride History)

---
+++ lib/models/past_trip.dart
@@ -0,0 +1,13 @@
+class PastTrip {
+  final String source;
+  final String destination;
+  final DateTime dateTime;
+  final double fare;
+
+  PastTrip({
+    required this.source,
+    required this.destination,
+    required this.dateTime,
+    required this.fare,
+  });
+}
