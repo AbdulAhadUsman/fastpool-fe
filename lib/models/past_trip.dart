897f20d3e8be45005a275bb39a749f8202ab8ea1
shariqmunir99 <shariq.munir7@gmail.com>
2025-05-07 07:21:24 +0500

    add past_trip model

--- lib/models/past_trip.dart
+++ lib/models/past_trip.dart
@@ -1,13 +1,43 @@
+import 'package:fastpool_fe/helper-functions/reverseGeoLoc.dart';
+
 class PastTrip {
-  final String source;
-  final String destination;
+  final int id;
+  final double sourceLat;
+  final double sourceLng;
+  final double destinationLat;
+  final double destinationLng;
   final DateTime dateTime;
-  final double fare;
+  String? sourceAddress;
+  String? destinationAddress;
+  bool isLoadingAddresses = true;
 
   PastTrip({
-    required this.source,
-    required this.destination,
+    required this.id,
+    required this.sourceLat,
+    required this.sourceLng,
+    required this.destinationLat,
+    required this.destinationLng,
     required this.dateTime,
-    required this.fare,
   });
+
+  factory PastTrip.fromJson(Map<String, dynamic> json) {
+    return PastTrip(
+      id: json['id'],
+      sourceLat: json['source_lat'].toDouble(),
+      sourceLng: json['source_lng'].toDouble(),
+      destinationLat: json['destination_lat'].toDouble(),
+      destinationLng: json['destination_lng'].toDouble(),
+      dateTime: DateTime.parse('${json['date']} ${json['time']}'),
+    );
+  }
+
+  Future<void> loadAddresses() async {
+    isLoadingAddresses = true;
+    sourceAddress = await getAddressFromLatLng(sourceLat, sourceLng);
+    destinationAddress = await getAddressFromLatLng(
+      destinationLat,
+      destinationLng,
+    );
+    isLoadingAddresses = false;
+  }
 }
